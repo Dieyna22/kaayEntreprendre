7161cfb402326634a4b9292f2977cf93e066e371
factorielle <dialobe110@gmail.com>
2023-12-07 17:10:27 +0000

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashEntrepreneurComponent } from './entrepreneur/dash-entrepreneur/dash-entrepreneur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashEntrepreneurComponent
   ],
   imports: [
     BrowserModule,
