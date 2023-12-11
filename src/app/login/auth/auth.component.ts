a31be471de788440dcc79ed2485f922938e03b2f
factorielle <dialobe110@gmail.com>
2023-12-11 15:02:06 +0000

    liaison avec le backend pour la connexion et l'inscription

--- src/app/login/auth/auth.component.ts
+++ src/app/login/auth/auth.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { EMPTY, catchError, tap } from 'rxjs';
+import { AuthService } from 'src/app/service/auth.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -7,10 +9,24 @@ import Swal from 'sweetalert2';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit{
 
   showSteps: boolean = true;
   choixFormulaire: boolean = true;
+  
+  user: any;
+  credentials: any;
+  loading!: boolean;
+  userConnected: any;
+  errorMsg: any;
+  experience: any;
+  realisation: any;
+  userConnect: any;
+
+  constructor(private route:Router, private authService: AuthService){}
+  ngOnInit(): void {
+    this.User();
+  }
 
   public afficherFormulaire() {
     this.choixFormulaire = !this.choixFormulaire;
@@ -25,10 +41,81 @@ export class AuthComponent {
   }
 
   // Nos attributs
+
+  activite:string='';
+  image:string='';
   emailLogin: string = "";
   passwordLogin: string = "";
   experimente: string = "";
   novice: string = "";
+  prenom: string = "";
+  nom: string = "";
+  email: string = "";
+  password: string = "";
+  adresse: string = "";
+  region:string="";
+  statExp:string="";
+  statNov:string="";
+  users:any;
+
+  
+// connexion
+login() {
+  this.loading = true;
+  if (this.emailLogin == "" || this.passwordLogin == "") {
+    this.showMessage("error","Oops", "Veuillez remplir tout les champs");
+  } else {
+    this.authService.login(this.emailLogin, this.passwordLogin).pipe(
+      tap(() => {
+        this.userConnected = JSON.parse(localStorage.getItem('userConnected') || "");
+        // console.log(this.userConnected.role);
+
+        this.loading = false;
+
+        if (this.authService.isAdmin$ && this.emailLogin == "admin@gmail.com" && this.passwordLogin == "passer") {
+          this.route.navigate(['/accueilAdmin']);
+        }
+
+        if (this.authService.isExperimente$) {
+          this.route.navigate(['/accueilEntrepreneur']);
+        }
+
+        if (this.authService.isNovice$) {
+          this.route.navigate(['/acceuilUser']);
+        }
+      }),
+      catchError((error:any) => {
+        this.loading = false;
+        this.errorMsg = error.message;
+        return EMPTY;
+      })
+    ).subscribe();
+
+  }
+}
+
+User(){
+  this.authService.getUsers().subscribe((reponse:any)=>{
+    this.users=reponse;
+    console.log('tea', this.users)
+    if( this.users){
+      this.userConnect=this.users.find((element:any)=>element.email===this.emailLogin )
+
+    }
+    console.log(this.userConnect)
+  })
+
+}
+
+
+// sweetalert
+showMessage(icon:any, titre:any, texte:any){
+  Swal.fire({
+    icon: icon,
+    title: titre,
+    text: texte,
+  })
+}
 
 
   connexion() {
@@ -67,5 +154,79 @@ export class AuthComponent {
       title: title,
       text: text
     });
+  }
+
+// inscription
+  signNovice() {
+    this.user={
+      prenom:this.prenom,
+      nom:this.nom,
+      email:this.email,
+      adresse:this.adresse,
+      password:this.password,
+      region:this.region,
+      image:this.image
+      
+    }
+    alert("hi")
+    this.authService.inscriptionNovice(this.user).subscribe(
+      (response:any) => {
+        console.log(this.user)
+        alert("hO")
+       
+        const token = response.access_token;
+        // this.route.navigate(['/accueilUser'])
+      },
+      (error:any) => {
+        // Gérez les erreurs d'inscription.
+        console.error('Erreur d\'inscription :', error);
+      }
+    );
+  }
+// inscription
+  signExperimente() {
+    this.user={
+      prenom:this.prenom,
+      nom:this.nom,
+      email:this.email,
+      adresse:this.adresse,
+      password:this.password,
+      region:this.region,
+      realisation:this.realisation,
+      experience:this.experience,
+      activite:this.activite
+    }
+    this.authService.inscriptionExperimente(this.user).subscribe(
+      (response:any) => {
+       
+        const token = response.access_token;
+        this.route.navigate(['/login'])
+      },
+      (error:any) => {
+        // Gérez les erreurs d'inscription.
+        console.error('Erreur d\'inscription :', error);
+      }
+    );
+  }
+
+  // connexion
+  signIn() {
+    this.credentials={
+      email:this.emailLogin,
+      password:this.passwordLogin
+    }
+    this.authService.signIn(this.credentials).subscribe(
+      (response:any) => {
+        // Stockez le token dans un service ou dans le stockage local (localStorage).
+       
+       
+        console.log(this.credentials)
+        this.route.navigate(['/accueilUser'])
+      },
+      (error:any) => {
+        // Gérez les erreurs de connexion.
+        console.error('Erreur de connexion :', error);
+      }
+    );
   }
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,22 +1,81 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://127.0.0.1:8000';  // URL de votre backend
+  private apiUrl = 'http://127.0.0.1:8000/api';  // URL de votre backend
+  userObj: string='';
+  authToken: string='';
+  isAdmin$: any;
+  isExperimente$: any;
+  isNovice$: any;
+  isAuth$: any;
 
   constructor(private http: HttpClient) {}
+  // login
+  login(email: string, password: string) {
+    return this.http.post<{ status_body: string, token: string }>('http://localhost:8000/api/login', { email: email, password: password }).pipe(
+      tap(({ status_body, token }) => {
+        this.userObj = status_body;
+        this.authToken = token;
+        if (email == "dialobe110@gmail.com" && password == "Passer@123") {
+          this.isAdmin$.next(true);
+        } else
+          localStorage.setItem('token', JSON.stringify(this.authToken));
+        localStorage.setItem('userConnected', JSON.stringify(this.userObj));
+
+        let userCon = JSON.parse(localStorage.getItem('userConnected') || '')
+
+        if (userCon.role === "entrepreneur_experimente") {
+          this.isExperimente$.next(true)
+        } else if (userCon.role === "entrepreneur_novice") {
+          this.isNovice$.next(true);
+        } else {
+          this.showMessage("error", 'Oops',"Email ou mot de passe incorrecte")
+        }
+
+        this.isAuth$.next(true); // on met à je la val de isAuth$
+
+        console.log(this.isAdmin$)
+        console.log(this.isExperimente$)
+        console.log(this.isNovice$)
+      })
+    );
+  }
+
+  // connexion
+
+
+
+  // sweetalert
+  showMessage(icon:any, titre:any, texte:any){
+    Swal.fire({
+      icon: icon,
+      title: titre,
+      text: texte,
+    })
+}
 
   // inscription
-  signUp(user: any): Observable<any> {
+  inscriptionNovice(user: any): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/ajouter-utilisateur-entrepreneur-novice`, user);
   }
+  inscriptionExperimente(user: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/ajouter-utilisateur-entrepreneur-experimente`, user);
+  }
+ 
   // connexion
   signIn(credentials: any): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/login`, credentials);
   }
+  getUsers(){
+    return this.http.get(`${this.apiUrl}/liste_utilisateur`)
+  }
+
+
 }
 
